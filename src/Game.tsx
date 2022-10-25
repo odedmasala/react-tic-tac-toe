135d6a0e036fae51ad6baa2d6331743df987b762
odedmasala <odedmasala6@gmail.com>
2022-10-25 16:11:35 +0300

    update

--- src/Game.tsx
+++ src/Game.tsx
@@ -110,7 +110,7 @@ function Game() {
   };
 
   return (
-    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
+    <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-green-300 to-green-800">
       <h1 className="text-center text-5xl mb-4 font-display text-white">
         Tic Tac Toe
       </h1>
